7bd89595f00687641799a9eb6af34be20bcac7fe
이준용 <iiiuooyoo@gmail.com>
2023-03-13 12:55:24 +0900

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+## ari-ascii-art
+
+아리로 아스키 아트를 해보겠어요
+
+1. ascii-art
+2. imgToAscii
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <>
+            <div>어서오고</div>
+        </>
+    );
+}
